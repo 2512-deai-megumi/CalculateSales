1c68208939d7e5dd3b34d394da8551feb0a2d72b
deai_megumi <deai.megumi@alhinc.jp>
2025-12-09 14:12:34 +0900

    [Add]売上ファイルの集計,支店別集計ファイル出力機能の追加

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -1,10 +1,14 @@
 package jp.alhinc.calculate_sales;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -37,7 +41,58 @@ public class CalculateSales {
 		}
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		//ファイルパス
+		String path = args[0];
+		//一時的にファイル名を保管する配列
+		File[] tmpFiles = new File(path).listFiles();
+		//rcdファイル名を保管するリスト
+		List<File> rcdFiles = new ArrayList<>();
 
+
+		for(int i =0; i<tmpFiles.length; i++) {
+			String rcdFileName = tmpFiles[i].getName();
+			if(rcdFileName.matches("^[0-9]{8}.rcd$")) {
+				 rcdFiles.add(tmpFiles[i]);
+			}
+		}
+
+		for(int i = 0;i<rcdFiles.size(); i++) {
+			BufferedReader br = null;
+			try {
+			String fileName = rcdFiles.get(i).getName();
+			File rcdFile = new File(path, fileName);
+			FileReader fr = new FileReader(rcdFile);
+			br = new BufferedReader(fr);
+			String line;
+			List<String> tmpBranches= new ArrayList<String>();
+			while((line = br.readLine()) != null) {
+				tmpBranches.add(line);
+			}
+			String code = tmpBranches.get(0);
+			Long sales =  Long.parseLong(tmpBranches.get(1));
+			Long salesAmount = branchSales.get(code) + sales;
+			branchSales.replace(code, salesAmount);
+
+
+			}catch(IOException e) {
+				System.out.println(UNKNOWN_ERROR);
+				return;
+			}finally{
+				// ファイルを開いている場合
+				if(br != null) {
+					try {
+						// ファイルを閉じる
+						br.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+						return;
+					}
+				}
+			}
+
+
+
+	}
 
 
 		// 支店別集計ファイル書き込み処理
@@ -73,11 +128,10 @@ public class CalculateSales {
 
 				branchNames.put(branches[0], branches[1]);
 				branchSales.put(branches[0], (long) 0);
-				System.out.println(branchNames);
 			}
 
 		} catch(IOException e) {
-			System.out.println(UNKNOWN_ERROR);
+			System.out.println(FILE_NOT_EXIST);
 			return false;
 		} finally {
 			// ファイルを開いている場合
@@ -105,6 +159,33 @@ public class CalculateSales {
 	 */
 	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
 		// ※ここに書き込み処理を作成してください。(処理内容3-1)
+		File file = new File(path, fileName);
+		BufferedWriter bw = null;
+
+		try {
+			FileWriter filewriter = new FileWriter(file);
+			bw = new BufferedWriter(filewriter);
+			//出力ファイルに書き込む
+			for(String key: branchNames.keySet()) {
+				bw.write(key + "," + branchNames.get(key) + "," + branchSales.get(key));
+				bw.newLine();
+
+			}
+
+		}catch(IOException e) {
+			System.out.println(UNKNOWN_ERROR);
+			return false;
+		}finally {
+			if(bw != null) {
+				try{
+					bw.close();
+				}catch(IOException e) {
+					System.out.println(UNKNOWN_ERROR);
+					return false;
+				}
+			}
+		}
+
 
 		return true;
 	}
